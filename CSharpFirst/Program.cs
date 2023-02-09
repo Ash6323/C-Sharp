6ece31fc1c5439532255e7cdc49343ef885cdc11
Ash6323 <ashwin6323@gmail.com>
2023-02-09 15:56:35 +0530

    Adding after making changes to .gitignore

---
+++ CSharpFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using ReadWrite;
+using Properties;
+
+internal class Program
+{
+    private static void Main (string[] args)
+    {
+        var objFirst = new MyReadWrite();
+        objFirst.ReadWrite();
+        
+        var objSecond = new MyProperties();
+        objSecond.Properties();
+     
+        Console.ReadKey();
+    }
+}

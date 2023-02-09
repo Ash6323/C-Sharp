e103a570d13f3d9d150017273ccbb69e15bb4a2f
Ash6323 <ashwin6323@gmail.com>
2023-02-09 17:10:52 +0530

    Added DataTypes file

---
+++ CSharpFirst/DataTypes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DataTypes
+{
+    public class MyDataTypes
+    {
+        public void DataTypes()
+        {
+            //Char
+            char ch = 'q';
+            Console.WriteLine($"ASCII of {ch} is {(byte) ch}");
+            Console.WriteLine($"Size of char is: {sizeof(char)} Byte");
+            Console.WriteLine();
+
+            //Byte
+            byte number = 25;
+            Console.WriteLine($"ASCII corresponding {number} is {(char)number}");
+            Console.WriteLine($"Size of byte is: {sizeof(byte)} Byte");
+            Console.WriteLine();
+
+            //String
+            string str = "Ashwin";
+            Console.WriteLine($"Entered String: {str}");
+            Console.WriteLine($"Size of entered string is: {str.Length * sizeof(char)} Byte");
+            Console.WriteLine();
+
+            //Int
+            int integerNumber = 67;
+            Console.WriteLine($"Entered Integer: {integerNumber}");
+            Console.WriteLine($"Size of int: {sizeof(int)} Byte");
+            Console.WriteLine($"Max Value of int: {int.MaxValue}");
+            Console.WriteLine();
+
+
+
+
+
+        }
+    }
+}
+
--- CSharpFirst/Program.cs
+++ CSharpFirst/Program.cs
@@ -1,17 +1,21 @@
 ﻿
 using ReadWrite;
 using Properties;
+using DataTypes;
 
 internal class Program
 {
     private static void Main (string[] args)
     {
-        var objFirst = new MyReadWrite();
+        /*var objFirst = new MyReadWrite();
         objFirst.ReadWrite();
-        
+
         var objSecond = new MyProperties();
-        objSecond.Properties();
-     
+        objSecond.Properties();*/
+
+        var objThird = new MyDataTypes();
+        objThird.DataTypes();
+
         Console.ReadKey();
     }
 }

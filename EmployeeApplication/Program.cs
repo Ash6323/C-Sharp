0d48274e2af66f540135e425b894c886253377a2
Ash6323 <ashwin6323@gmail.com>
2023-02-13 14:47:52 +0530

    to check changes

---
+++ EmployeeApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

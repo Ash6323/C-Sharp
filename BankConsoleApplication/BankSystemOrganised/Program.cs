c1ab84a2f29b43810b41dca1f4b731f4a05a3c42
Ash6323 <ashwin6323@gmail.com>
2023-02-13 14:08:42 +0530

    Changes to BankSystem gitignore

---
+++ BankConsoleApplication/BankSystemOrganised/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+using InnerSystem;
+
+namespace BankSystemOrganised
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankSystem bankObj = new BankSystem();
+            bankObj.run();
+        }
+    }
+
+}

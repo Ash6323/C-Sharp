1618e4eb81662d9f6fd892a91cfe01c574b1178c
Ash6323 <ashwin6323@gmail.com>
2023-03-10 23:20:53 +0530

    changes

--- FileUploadProgressNew/ConstantsFile.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FileUploadProgressNew
-{
-    public static class ConstantStringsClass
-    {
-        public static string FileDoesNotExist = "File Does Not Exist";
-        public static string UploadDirectory = "D:\\Folder";
-    }
-}
